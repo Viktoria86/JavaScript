a44925d4547d18905c272dd82dabba29d6c973d9
Viktoria86 <vika.zh86@bk.ru>
2021-12-05 16:15:41 +0300

    Список

---
+++ Список/1.js
@@ -0,0 +1,19 @@
+const input = document.querySelector('.input');
+const btn = document.querySelector('.btn');
+const list = document.querySelector('.list');
+
+btn.addEventListener('click', function(event) {
+let li = document.createElement('li');
+li.innerHTML = input.value;
+  if(li.innerHTML>10) {
+     li.className = "red";
+}
+  if(li.innerHTML<10) {
+     li.className = "green";
+}
+  if(li.innerHTML==10) {
+     li.className = "blue";
+}
+  
+list.appendChild(li);
+});
